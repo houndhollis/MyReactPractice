e38b402bb0c9d705acfa77537db68bc927f22f59
열정영웅 <103996469+houndhollis@users.noreply.github.com>
2022-07-24 22:28:42 +0900

    React +styleComponent

---
+++ HomePageClone/src/components/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='Main'>
+      <img src='https://occ-0-3683-988.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABfzN-oJgCYewW4CNGT42qxYWaODZdEklFR6pGzVOktBmBpH_khaAaBsW-LSDLPV78Kt-vmuFW6yRsipCbR4d__Mg5rJSLwgl1dQIYh0f4eB8.webp?r=65d'/>
+      <div className='Main_p'>고무고무 열매를 먹고 고무 인간이 된 루피. 해적왕을 꿈꾸는 그의 곁엔 검객 조로, 항해사 나미, 저격수 우솝, 요리사 상디 등 밀짚모자 일당이 있다. 위대한 항로를 따라 대비보 '원피스'를 찾아가는 루피의 모험을 그렸다.</div>
+      <div className='Main_btn'>
+        <button className='btn1'>
+            <img src='https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon5/mt-play_arrow.svg'/>
+            <span>재생</span>
+        </button>    
+        <button className='btn2'>
+            <img src='https://raw.githubusercontent.com/eirikmadland/notion-icons/master/v5/icon1/mt-info_outline.svg'/>
+            <span>상세 정보</span>
+        </button>    
+      </div>   
+    </div>
+  )
+}
+
+export default Main
